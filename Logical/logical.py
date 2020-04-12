337f7a0bdf1389826cf2cb0be0acdd2b03b0ee7e
farhanpro <shaikhfarhan549@gmail.com>
2020-04-12 14:39:23 +0530

    Third Day

--- Logical/logical.py
+++ Logical/logical.py
@@ -1,7 +1,7 @@
 age = int(input("Enter you age to place your Vote : "))
-if age >= 18:
+if age >= 18 and age > 0:
     print("You are Eligible to place your vote.  ")
-elif age <= 18:
+elif age <= 18  and age > 0 :
     print("You are Not Eligible to place your vote.  ")
-#elif age < 0 :
- #   print("Negative number inserted.")
+else:
+    print("Negative number inserted")
