e07f2b198922f9d5749ec3fa2070c472ff16c4bf
farhanpro <shaikhfarhan549@gmail.com>
2020-04-12 14:04:06 +0530

    Third Day

---
+++ Assingment/assing_ops.py
@@ -0,0 +1,28 @@
+# Assingment operators are used to assing a value to a operator
+# There are 8 types of operator
+# 1) =
+# 2) +=
+# 3) -=
+# 4) *=
+# 5) /=
+# 6) %=
+# 7) //=
+
+x = int(input("Enter a Number : "))
+x+=2 
+print(x)
+
+x-=2
+print(x)
+
+x*=3
+print(x)
+
+x/=2
+print(x)
+
+x%=2
+print(x)
+
+x//=2
+print(x)
---
+++ Comparision/Comparision.py
@@ -0,0 +1,7 @@
+age = int(input("Enter you age to place your Vote : "))
+if age >= 18:
+    print("You are Eligible to place your vote.  ")
+elif age <= 18  age > 0 :
+    print("You are Not Eligible to place your vote.  ")
+#elif age < 0 :
+ #   print("Negative number inserted.")
