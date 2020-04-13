35b52d3bd5184d2a15d3820cc8d4549d6edf9940
farhanpro <shaikhfarhan549@gmail.com>
2020-04-13 20:27:18 +0530

    Passing Direct values

---
+++ functions/hours_converter2.py
@@ -0,0 +1,9 @@
+def minutes(minutes):
+   # minutes = int(input("Enter minutes to convert into Hours :"))
+    return print("Converted value is : ",minutes/60)
+
+def second(second):
+    return print("Converted value is : ",second/3600)
+
+minutes(90)
+second(6000)
