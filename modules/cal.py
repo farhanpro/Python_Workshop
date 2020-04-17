4669fd324d531ed7637302482fbb1a5f7fff2ed1
farhanpro <shaikhfarhan549@gmail.com>
2020-04-17 12:01:06 +0530

    Uploded first project

---
+++ modules/cal.py
@@ -0,0 +1,9 @@
+def add(a,b):
+    return print(a+b)
+
+def sub(x,y):
+    return print(x+y)
+
+def mul(i,j):
+    return print(i*j)
+
---
+++ modules/work.py
@@ -0,0 +1,8 @@
+import cal
+from cal import add
+from cal import sub
+from cal import mul
+
+add(50,30)
+sub(100,80)
+mul(5,5)
