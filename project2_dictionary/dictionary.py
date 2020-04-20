32e87668ec3fe5abc246708da72007dc418953c4
farhanpro <shaikhfarhan549@gmail.com>
2020-04-20 11:34:18 +0530

    Corrections made in dictionary.py now if you entered a wrong keyword it will tell you to correct it.

--- project2_dictionary/dictionary.py
+++ project2_dictionary/dictionary.py
@@ -1,13 +1,21 @@
 import json
+data = json.load(open("original.json")) 
 def dict():
-   
-    data = json.load(open("original.json"))
     word = input("Enter the word you want to search : ")
     converted = word.casefold()
-    print(data[converted])
+    if converted in data or type(converted) == list :   #Correction made  
+        num = 0
+        for item in data[converted]:
+             num = num+1
+             print(num,item)
+    else:
+        print("Check the word you Entered")
+    
 
 dict()
+    
 i = 1
+
 while (i==1):
     decide = input("You want to search another word?(Y/N)")
     if decide == 'Y' or decide == 'y':
